a824e2e82e72f3b83602e8e73301926b02a4653b
peedro367 <pc621528@gmail.com>
2026-05-04 09:34:28 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,7 @@
+letra = input("digite a letra")
+if letra == "A";
+ print(voce escolheu a letra a")
+elif letra == "B":
+ print("voce escolheu a letra b")
+else:
+ print("essa opcao nao esta disponivel")
